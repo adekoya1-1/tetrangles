ff1622ec8eedf5e79c59d884523002ceb4c3886b
adekoya1-1 <adekoyas73@gmial.com>
2025-06-19 13:28:19 +0100

    added more content to the about page

---
+++ src/components/AboutComponenet/Quote.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Quote = () => {
+  return (
+    <div className="quote-container">
+      <div>
+        <h3 className="quote-text">“Suspendisse neque erat, imperdiet ac non, sollicitudin accumsan lacus. Vestibulum ac ex rutrum, pellentesque purus et, lacinia mi. Nullam maximus lectus libero.”</h3>
+        <h6 className="quote-author">ADEKOYA NURUDEEN.O. - CEO</h6>
+      </div>
+    </div>
+  )
+}
+
+export default Quote
